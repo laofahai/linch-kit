fb23a69f1df108c7389131255db6ac5911864137
laofahai <laofahai@gmail.com>
2025-06-16 16:41:08 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'import', 'jsx-a11y'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'plugin:jsx-a11y/recommended',
+        'prettier',
+    ],
+    rules: {
+        // 你可以根据需求调整规则，比如：
+        'react/react-in-jsx-scope': 'off', // Next.js 不需要显式引入 React
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'import/order': ['error', { 'newlines-between': 'always' }],
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {},
+        },
+        {
+            files: ['*.js', '*.jsx'],
+            rules: {},
+        },
+    ],
+}
