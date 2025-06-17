77e4dee3479ab7ce3eb4cd575782fee938666808
laofahai <laofahai@gmail.com>
2025-06-17 23:25:15 +0800

    @linch-kit/schema

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -8,9 +8,3 @@ export * from './generators/prisma'
 export * from './generators/validators'
 export * from './generators/mock'
 export * from './generators/openapi'
-
-// Re-export zod for convenience
-export { z } from 'zod'
-
-// Version
-export const version = '0.1.0'
