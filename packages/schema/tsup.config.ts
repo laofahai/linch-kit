c857249b04e239d41785382db736dde5adba94f5
laofahai <laofahai@gmail.com>
2025-06-17 23:33:44 +0800

    @linch-kit/schema

--- packages/schema/tsup.config.ts
+++ packages/schema/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: {
     index: 'src/index.ts',
-    'validators/index': 'src/validators/index.ts',
     'cli/index': 'src/cli/index.ts',
   },
   format: ['esm', 'cjs'],
